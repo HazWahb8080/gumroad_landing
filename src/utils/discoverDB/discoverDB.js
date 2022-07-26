91976fa324d6da4ec9c6f510b22f88ff1b3b180d
HazWahb8080 <hazem.wahb100@gmail.com>
2022-07-26 13:25:33 +0200

    added --- DB drkt for the discover secttion elements

---
+++ src/utils/discoverDB/discoverDB.js
@@ -0,0 +1,8 @@
+export const discoverDB = [
+    {
+        title:"Animation",
+        desc:"Perfect your craft with the same tools used at Dreamworks and Pixar.",
+        icon:"https://ucarecdn.com/15b40295-8ba7-4462-bd11-d7b4c6353922/animation.svg",
+        tags:["blender","spark ar","3d model"]
+    },
+]
